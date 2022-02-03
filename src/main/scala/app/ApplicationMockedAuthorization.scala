6a4635b0f37d0684013c5e10fa4920edd1b79333
mtumilowicz <michal.tumilowicz01@gmail.com>
2022-02-03 18:10:42 +0100

    mocked authorization

--- src/main/scala/app/ApplicationMockedAuthorization.scala
+++ src/main/scala/app/ApplicationMockedAuthorization.scala
@@ -7,15 +7,16 @@ import app.gateway.HttpApi
 import app.infrastructure.authorization.PublicKeyInMemoryRepository
 import app.infrastructure.product.ProductModule
 import app.infrastructure.user.UserModule
+import cats.data.Kleisli
 import cats.effect._
 import com.comcast.ip4s.Port
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.server.Server
 import org.http4s.server.defaults.Banner
+import org.http4s.server.{AuthMiddleware, Server}
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
-object Application extends IOApp {
+object ApplicationMockedAuthorization extends IOApp {
 
   private def showEmberBanner[F[_] : Logger](s: Server): F[Unit] =
     Logger[F].info(s"\n${Banner.mkString("\n")}\nHTTP Server started at ${s.address}")
@@ -23,11 +24,11 @@ object Application extends IOApp {
   // for the sake of simplicity - probably will be fetched from the authorization server
   private val jwtPublicKey =
     """-----BEGIN PUBLIC KEY-----
-      |MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCCjk/HJbdaoZqIq8ZIien3wxqP
-      |4jwJRXTMu6s95FYZm2ADr6ROE5gPIxX0RmTFN1lyvAEtZbQgKG63TJiPHgYQu8RD
-      |31ERe4X0pXpDoTEiinyVy7j2aL8s+2aFe0c/X3Ny4Hnk+y1S5qlKPgrLV5bbylLZ
-      |/Ml3+ofZ+HgIntpKcQIDAQAB
-      |-----END PUBLIC KEY-----""".stripMargin
+    |MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCCjk/HJbdaoZqIq8ZIien3wxqP
+    |4jwJRXTMu6s95FYZm2ADr6ROE5gPIxX0RmTFN1lyvAEtZbQgKG63TJiPHgYQu8RD
+    |31ERe4X0pXpDoTEiinyVy7j2aL8s+2aFe0c/X3Ny4Hnk+y1S5qlKPgrLV5bbylLZ
+    |/Ml3+ofZ+HgIntpKcQIDAQAB
+    |-----END PUBLIC KEY-----""".stripMargin
 
   private val users = List(
     User("user1", Permissions(Set(Permission.Product))),
@@ -38,17 +39,13 @@ object Application extends IOApp {
     Product("1", "product 1")
   )
 
-  private def prepareAuthorizationService(): IO[AuthorizationService[IO]] = for {
-    jwtUserAuth <- JwtPublicKeyService(PublicKeyInMemoryRepository[IO](jwtPublicKey)).fetchUserAuth()
-    userService = UserModule.inMemoryService[IO](users)
-  } yield AuthorizationService(jwtUserAuth, userService)
+  private def mockedAuthMiddleware(): AuthMiddleware[IO, User] =
+    AuthMiddleware[IO, User](Kleisli.pure(User("user1", Permissions(Set(Permission.Product)))))
 
   override def run(args: List[String]): IO[ExitCode] = for {
     logger <- Slf4jLogger.create[IO]
-    authorizationService <- prepareAuthorizationService()
-    authMiddleware = AuthMiddleware[IO, User](authorizationService)
     productService = ProductModule.inMemoryService[IO](products)
-    api = HttpApi[IO](authMiddleware, productService).httpApp
+    api = HttpApi[IO](mockedAuthMiddleware(), productService).httpApp
     server <- EmberServerBuilder.default[IO]
       .withPort(Port.fromInt(9090).get)
       .withHttpApp(api)
